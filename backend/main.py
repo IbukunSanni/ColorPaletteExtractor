24608b919da9f35447f44095cd60f40f89e5b952
IbukunSanni <ibukun.sanni@gmail.com>
2025-05-01 18:21:28 -0400

    updated cors

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,9 @@ app = FastAPI()
 # Allow CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace with your frontend URL in production
+    allow_origins=[
+        "http://localhost:3000/"
+    ],  # Replace with your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
