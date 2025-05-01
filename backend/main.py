1674077a1833817707675855be016567ffdcd52b
IbukunSanni <ibukun.sanni@gmail.com>
2025-05-01 18:49:09 -0400

    updated for cors with correction

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000/"
+        "http://localhost:3000"
     ],  # Replace with your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
